3925a8cc72d9ec2de0b08155e990557196350e63
Renan <renanroberto1@gmail.com>
2020-05-24 19:15:20 -0300

    improve Answer module
    - add more question marks
    - add glitch answer for ‽
    - improve question detection

--- src/Answer.hs
+++ src/Answer.hs
@@ -5,14 +5,17 @@ module Answer where
 import SafeName (SafeName)
 
 
+questionMarks :: [Char]
+questionMarks = ['?', '¿', '‽', ';', '՞', '؟', '፧', '⁇', '⁈', '⁉']
+
 isQuestion :: String -> Bool
-isQuestion ('?':str) = all (`elem` ['?', '!']) str
-isQuestion "¿" = True
-isQuestion "‽" = True
-isQuestion _ = False
+isQuestion "" = False
+isQuestion (x:xs) =
+  x `elem` questionMarks && all (`elem` '!':questionMarks) xs
 
 getAnswer :: String -> Maybe SafeName -> String
 getAnswer "¿" _ = "¿ɐpᴉʌn̗p ɐns ɐ ǝ̗ ʅɐnꝹ" -- Boa noite, Bruno
+getAnswer "‽" _ = "w̶h̷a̸t̶ ̸i̸s̷ ̷y̸o̷u̸r̵ ̸q̵u̶e̵s̶t̵i̴o̸n̴‽̶"
 getAnswer _ Nothing = "Qual é a sua dúvida?"
 getAnswer marks (Just name) =
   "Qual é a sua dúvida, " ++ show name ++ (take 10 marks)

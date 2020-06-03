7a98370cacff42c2e6172af15eb5e52e1fc5902c
Renan <renanroberto1@gmail.com>
2020-06-02 22:59:13 -0300

    add global state (not working)

--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,7 @@ module Main where
 import Text.Read (readMaybe)
 import Data.Maybe (fromMaybe)
 import System.Environment (lookupEnv)
+import Control.Monad.Trans.State.Lazy hiding (State)
 import Control.Monad.IO.Class (liftIO)
 import Servant
 import Network.Wai.Handler.Warp (run)
@@ -20,9 +21,25 @@ type BotAPI =
     :> Capture "token" String
     :> Post '[JSON] NoContent
 
+type BookAPI =
+  "book"
+    :> Capture "book" String
+    :> Get '[JSON] [String]
+
 type API = RootAPI
            :<|> BotAPI
+           :<|> BookAPI
 
+
+data State = State
+  { books :: [String]
+  }
+
+type AppM = StateT State Handler
+
+
+initialState :: State
+initialState = State []
 
 getPort :: IO Int
 getPort = do
@@ -35,30 +52,41 @@ getToken = do
   return $ fromMaybe "" env
 
 
-rootHandler :: Handler String
+rootHandler :: AppM String
 rootHandler = return "online"
 
-botHandler :: Update -> String -> Handler NoContent
+botHandler :: Update -> String -> AppM NoContent
 botHandler update token = do
   envToken <- liftIO getToken
   if envToken == token
     then liftIO (bot update) >> return NoContent
     else return NoContent
 
+bookHandler :: String -> AppM [String]
+bookHandler book = do
+  state <- get
+  let newState = State $ book : (books state)
+  put newState
+  return (books newState)
 
-server :: Server API
+
+server :: ServerT API AppM
 server = rootHandler
          :<|> botHandler
+         :<|> bookHandler
 
 api :: Proxy API
 api = Proxy
 
-app :: Application
-app = serve api server
+nt :: State -> AppM a -> Handler a
+nt s x = evalStateT x s
+
+app :: State -> Application
+app state = serve api $ hoistServer api (nt state) server
 
 
 main :: IO ()
 main = do
   port <- getPort
   putStrLn $ "Server is running on port " ++ (show port)
-  run port app
+  run port (app initialState)

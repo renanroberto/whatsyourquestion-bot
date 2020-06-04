72e2cffb0224b36b0fe7c34fe982cfd55318d1dd
Renan <renanroberto1@gmail.com>
2020-06-03 22:18:43 -0300

    add spanish and inverted glitch easter eggs

--- src/Answer.hs
+++ src/Answer.hs
@@ -14,8 +14,12 @@ isQuestion (x:xs) =
   x `elem` questionMarks && all (`elem` '!':questionMarks) xs
 
 getAnswer :: String -> Maybe SafeName -> String
-getAnswer "¿" _ = "¿ɐpᴉʌn̗p ɐns ɐ ǝ̗ ʅɐnꝹ" -- Boa noite, Bruno
-getAnswer "‽" _ = "Q̶̥̎u̸͉͒å̸͜l̷̗̀ ̷̺̈é̸̗͆ ̸̘́a̴̧̔ ̴̜̍s̶̠̃u̴͌ͅa̴̦̾ ̷͆͜d̷̪͝ú̷͊͜v̷͍̽ī̴͇d̶̖̚a̴̩͐‽̵̟̉"
+getAnswer "¿"  _ = "¿ɐpᴉʌn̗p ɐns ɐ ǝ̗ ʅɐnꝹ" -- Boa noite, Bruno
+getAnswer "¿?" _ = "¿Cuál es tu pregunta?"
+getAnswer "‽"  _ =
+  "Q̶̥̎u̸͉͒å̸͜l̷̗̀ ̷̺̈é̸̗͆ ̸̘́a̴̧̔ ̴̜̍s̶̠̃u̴͌ͅa̴̦̾ ̷͆͜d̷̪͝ú̷͊͜v̷͍̽ī̴͇d̶̖̚a̴̩͐‽̵̟̉"
+getAnswer "⸘"  _ =
+  "¿̴̠͗ɐ̶̧̽p̸͕̀ᴉ̵̳͛ʌ̷̘͐n̶̼̚p̷̠̍ ̴̜̔ɐ̷̟̓n̸͙͊s̴̢̀ ̸̜̽ɐ̴͍̈ ̸̱̉ǝ̷̞͋ ̶̻̈ʅ̷͙͠ɐ̶͖̐n̴͔̿Ꝺ̸̮̃"
 getAnswer _ Nothing = "Qual é a sua dúvida?"
 getAnswer marks (Just name) =
   "Qual é a sua dúvida, " ++ show name ++ (take 10 marks)
